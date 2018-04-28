4239b9c5f70aa62b6fdb61071fd5eb31eb956445
Nicolo Solano <nicolo.solano@kambi.com>
2018-04-28 16:56:16 +0200

    Added other steps

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# reuters-21578
-reuters-21578
+# Classification of Reuters-21578 
+
+### Requirements
+<br />
+To install these packages, use the following command in a <a href="http://docs.python-guide.org/en/latest/dev/virtualenvs/" target="_blank"> virtualenv</a>.
+```bash
+$ pip3 install -r requirements.txt
+```
+In alternative, install the packages listed in requirement.txt file manually
+NOTE: The code works only with python >= 3.5
+
+### Training data
+.
+<br />
+Available in sgm format in:
+```bash
+data/ 
+```
+
+### Train 
+
+To train one of the implemented algorithms, run the following command:
+
+```bash
+$ python train_reuters.py 
+```
+Flags
+```bash
+--linearsvm            # use Linear Support Vector Machine classifier 
+--knn                  # use Naive-Bayes
+--logisticregression   # use Perceptron
+--randomforest         # use Random Forest
+```
+
+NOTE: The presence of a flag is mandatory.
+
+
+### Test 
+To test one of the implemented algorithms, run the following command:
+
+```bash
+$ python test_reuters.py 
+```
+Flags
+```bash
+--linearsvm            # use Linear Support Vector Machine classifier 
+--knn                  # use Naive-Bayes
+--logisticregression   # use Perceptron
+--randomforest         # use Random Forest
+```
--- reuters_preprocessor.py
+++ reuters_preprocessor.py
@@ -23,20 +23,19 @@ class ReutersPreprocessor():
         """
         Given a text, returns a list of tokens according to the following rules:
         1 - apply a specified stemming algorithm, 
-        2 - remove stop words and consider only alpha-numeric characters
-        3 - consider only tokens with length >= min_length 
+        2 - remove stop words
+        3 - consider only tokens with alphabetic characters and length >= min_length 
         """
         words = map(lambda word: word.lower(), word_tokenize(text))
         words = [word for word in words if word not in self.stopwords]
-        tokens = (list(map(lambda token: PorterStemmer().stem(token),words)))
+        tokens = list(map(lambda token: PorterStemmer().stem(token),words))
         filtered_tokens = list(filter (lambda token: re.match('[a-zA-Z]+', token) and len(token) >= self.min_length,tokens))
         return filtered_tokens
 
         
     def pre_process(self, documents):
         """
-        Preprocess the reuters-21578 document collection and returns
-        train and test.
+        Preprocess the reuters-21578 document collection and returns train and test.
         """
                
         train_documents = documents[(documents.lewissplit == "TRAIN")]
@@ -45,17 +44,17 @@ class ReutersPreprocessor():
         train_category_set =  set(chain(*train_category_list)) 
         test_documents = documents[(documents.lewissplit == "TEST")]
 
-        #From the test set, we need to remove the topics that are not present in the train set
+        #From the test set we need to remove the topics that are not present in the train set, if any
         test_documents.loc[:,"topics"] = test_documents.topics.apply(lambda x: [entry for entry in x if entry in train_category_set])
         
         #Vectorize the data using the tf-idf method
         vectorizer = TfidfVectorizer(tokenizer=self.tokenize,min_df=2)
-        vec_train_documents = vectorizer.fit_transform(train_documents["text"])
-        vec_test_documents = vectorizer.transform(test_documents["text"])
+        vectorized_train_documents = vectorizer.fit_transform(train_documents["text"])
+        vectorized_test_documents = vectorizer.transform(test_documents["text"])
  
         # Transform labels for multi label classification
         mlb = MultiLabelBinarizer()
         train_labels = mlb.fit_transform([doc for doc in train_documents["topics"]])
         test_labels = mlb.transform([doc for doc in test_documents["topics"]])
         
-        return vec_train_documents, vec_test_documents, train_labels, test_labels
+        return vectorized_train_documents, train_labels, vectorized_test_documents, test_labels
